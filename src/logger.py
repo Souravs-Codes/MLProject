91529b5e8455fecf790562482c8b903536f1fbf2
Souravs-Codes <souravmukherjee1584@gmail.com>
2026-07-18 09:48:55 +0530

    Debugging

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,15 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(log_path,exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
-
